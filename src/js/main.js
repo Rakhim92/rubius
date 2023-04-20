005e5def44865902d1c9bd1f9600fd50fc327874
Abdraev Rakhim 17.11.1992 <pharmamystery19@gmail.com>
2023-04-20 16:11:10 +0700

    Выполнил все задания 4 спринта. Пока без обработки

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,14 @@
 
+Fancybox.bind('[data-fancybox="gallery"]', {
+  // Your custom options
+});
 
 const hamb = document.querySelector("#hamb");
 const popup = document.querySelector("#popup");
 const body = document.body;
 
 // // Клонируем меню, чтобы задать свои стили для мобильной версии
-// const menu = document.querySelector("#menu").cloneNode(1);
+// const menu = document.querySelector("#popup").cloneNode(1);
 
 // При клике на иконку hamb вызываем ф-ию hambHandler
 hamb.addEventListener("click", hambHandler);
@@ -39,3 +42,45 @@ function hambHandler(e) {
 //   hamb.classList.remove("active");
 //   body.classList.remove("noscroll");
 // }
+
+const pricesInner = document.querySelector('.prices__inner');
+const tabs = document.querySelectorAll('.prices__type');
+const tabsContent = document.querySelectorAll('.prices__checklist');
+
+tabsContent[0].classList.add('prices--active')
+pricesInner.addEventListener('click', function(event) {
+
+
+//Предварительная очистка всех табов от класса prices__selected
+  Array.from(tabs).forEach( item => {
+    item.classList.remove('prices__selected')
+  });
+//И очистка связанного с табами содержимого от класса prices__active
+  Array.from(tabsContent).forEach( item => {
+    item.classList.remove('prices--active')
+  })
+
+//Добавление выбранному табу класса prices__selected
+  event.target.classList.add('prices__selected');
+  
+//И добавление класса prices--active. Отображение контента согласно индексу
+  let index = [...tabs].indexOf(event.target);
+  if (index === -1) {
+    index = 0;
+  }
+  tabsContent[index].classList.add('prices--active')
+ 
+})
+
+const contactsForm = document.getElementById('menu__form');
+
+contactsForm.addEventListener('submit', function(event) {
+  event.preventDefault();
+  const {name, phone} = event.target.elements
+  console.log({
+    name: name.value,
+    phone: phone.value
+  })
+  this.reset();
+})
+
